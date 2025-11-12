5c38ddf3c8994eca6af49972099be6bcc9ef23d7
Sergio <serizquierdoprof@gmail.com>
2025-11-12 20:00:19 +0100

    update collection code

--- staff/sergio-izquierdo/playground/js/collection.js
+++ staff/sergio-izquierdo/playground/js/collection.js
@@ -39,12 +39,10 @@ console.log(collection)
 collection.add('Pepito')
 console.log(collection)
 
-collection.update = function (src, update){
-    for(var i = 0; i < this.count; i++){
-        if(this[i] === src)
-            this[i] = update
-    }
-
+collection.update = function (target, replacement){
+    for(var i = 0; i < this.count; i++)
+        if(this[i] === target)
+            this[i] = replacement
 }
 collection.update('Pepito', 'Jiminy')
 console.log(collection)
