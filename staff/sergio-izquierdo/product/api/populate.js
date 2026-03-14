c7d5ea718fa701c6a8aeb514da7caa5d2d0bfb7a
Sergio <serizquierdoprof@gmail.com>
2026-03-14 19:16:21 +0100

    add mongodb client and use it in populate

--- staff/sergio-izquierdo/product/api/populate.js
+++ staff/sergio-izquierdo/product/api/populate.js
@@ -1,20 +1,37 @@
-import  { data, User, Pet} from './data.js'
+import { MongoClient, ObjectId } from 'mongodb'
 
-data.insertUser(new User('user-' + data.usersCount, 'Jack Skellington', 'jack@haloween.com', 'jack', '123123123', 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnliYm5zbHhtcGVobG8zeGdjeDA3Ynd4ZWk1MXBwa3c4aG1nOGx4OCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/K4Ov4XqbqUSf6/giphy.gif','regular'))
-data.insertUser(new User('user-' + data.usersCount, 'Shally', 'shally@haloween.com', 'shally', '123123123', null, 'regular'))
-data.insertUser(new User('user-' + data.usersCount, 'Sandy Claws', 'sandy@christmas.com', 'claws', '123123123', null,'regular'))
-data.insertUser(new User('user-' + data.usersCount, 'Oogie Boogie', 'oogie@boogie.com', 'oogieboogie', '123123123', null, 'regular'))
+const client = new MongoClient('mongodb://localhost:27017') //creamos un cliente que se va a conectar ahí, la dirección de mongo en nuestra máquina
 
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Zero', '2024-12-01', 0, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXcxNWd0enZhZjBxMzB0NGhyNnFqN3ZhdHVyMXd3aWpmaXdicGlveCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Bo95o8mSV8jHW/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Scraps', '2020-12-01', 1, 'https://imgs.search.brave.com/1VVPWkEK7nOD4Y0qAHwieXRR7edFknS3eBPyxpBdebk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9iLnRo/dW1icy5yZWRkaXRt/ZWRpYS5jb20vVkI4/dThyNkdpdUt6V2NM/aWZuQmcwSHk4RDA1/SE9CWlRNSHpCajFr/ekZ6VS5qcGc'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Sparky', '2014-12-01', 8, 'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3dzV0ZTkzN3ZyN2g2NTUzMWVhNWEzMXRndmo5ZGpiMHZlMzV1YThheiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/VEdrOxVXfkgRW/giphy.gif'))
+client.connect()
+	.then(() => {   //si ha ido bien la conexión, elegimos base de datos, product en nuestro caso
+		const db = client.db('product')
 
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-1', 'Mafia', '2025-07-01', 1.5, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnI3Z3J0c3lybjFwcmVpMjVoN29nNXQzNml2ZTNjbXZ3NHdncTQ5MCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/901mxGLGQN2PyCQpoc/giphy.gif'))
+		const users = db.collection('users')
 
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-2', 'Dormilon', '2019-11-01', 4, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnI3Z3J0c3lybjFwcmVpMjVoN29nNXQzNml2ZTNjbXZ3NHdncTQ5MCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/v6aOjy0Qo1fIA/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-2', 'Espumita', '2024-10-01', 1.4, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGdmajV3cHltazV2Z2ttZGs0bXp3YjZ0dTRmamUwNWNuYTM2a2UyeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/cYZkY9HeKgofpQnOUl/giphy.gif'))
+		/* users.find({}).toArray()
+			.then(users => console.table(users))
+			.catch(error => console.error(error)) */
 
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-3', 'Osito', '2023-09-01', 3.5, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHQ3b2NjNDE3aW1rZGUwYTJsaXI4dzV6aGI5cGk0NmE4aGJ2cmhoMCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lNLqexL939DTyR0uH2/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-3', 'Negrita', '2022-06-01', 3.2, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHQ3b2NjNDE3aW1rZGUwYTJsaXI4dzV6aGI5cGk0NmE4aGJ2cmhoMCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Q60eJgzLbQUM6qFRQo/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-3', 'Sargento', '2022-09-01', .2, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzVpOXp6ZGRlNXdwdHh3Z28xODRzN3djMTl2ZTF2MHY1NGl4dW13NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3imh62nmqnKYutIGnJ/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-3', 'Egoista', '2023-09-01', .25, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzVpOXp6ZGRlNXdwdHh3Z28xODRzN3djMTl2ZTF2MHY1NGl4dW13NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/D74yIoiOstPA1e3xYL/giphy.gif'))
+		/* users.insertOne({ name: 'Le Chuga', email: 'le@chuga.com', username: 'lechuga', password: '123123123'})
+			.then(result => console.log(result))
+			.catch(error => console.error(error)) */
+
+		/* users.updateOne({_id: new ObjectId('69b43cb74749f0937d8563b3')}, {$set: {name: 'Sally Skellington'}})
+			.then(result => console.log(result))
+			.catch(error => console.error(error)) */
+
+		/* users.deleteOne({_id: new ObjectId('69b46f6157d8cf4f387d4371')})
+			.then(result => console.log(result))
+			.catch(error => console.error(error)) */
+
+		/* users.findOne({_id: new ObjectId('69b43cb74749f0937d8563b3')})
+			.then(user => console.log(user))
+			.catch(error => console.error(error)) */
+
+		users.find({name: /s/i }).toArray()
+			.then(users => console.table(users))
+			.catch(error => console.error(error))
+
+
+	})
+	.catch(error => console.error(error))
