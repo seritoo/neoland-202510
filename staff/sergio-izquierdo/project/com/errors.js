90bfb26cb8eb3fbcf75f0cd39ad5ab84fab1501d
Sergio <serizquierdoprof@gmail.com>
2026-04-15 21:06:49 +0200

    create com folder and convert it in local package; create api folders and install dependencies; implement errors, validate, regex and index in com folder

---
+++ staff/sergio-izquierdo/project/com/errors.js
@@ -0,0 +1,47 @@
+export class ValidationError extends Error {
+	constructor(message) {
+		super(message)
+
+		this.name = this.constructor.name
+	}
+}
+
+export class ExistenceError extends Error {
+	constructor(message) {
+		super(message)
+
+		this.name = this.constructor.name
+	}
+}
+
+export class DuplicityError extends Error {
+	constructor(message) {
+		super(message)
+
+		this.name = this.constructor.name
+	}
+}
+
+export class SystemError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+export class AuthError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+export const errorMap = {
+	ValidationError,
+	ExistenceError,
+	DuplicityError,
+	SystemError,
+	AuthError
+}
---
+++ staff/sergio-izquierdo/project/com/index.js
@@ -0,0 +1,3 @@
+export * from './errors.js'
+export * from './validate.js'
+export * from './regex.js'
